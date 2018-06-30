a0f5cb62beeb8756bc2b19487af0d60ab151d221
hd182 <1284287963@qq.com>
2018-06-30 18:02:41 +0800

    最新ai

--- src/TxAiAuthentication.js
+++ src/TxAiAuthentication.js
@@ -2,8 +2,8 @@ const crypto = require('crypto');
 const request=require('request');
 const fs=require('fs')
 const config = {
-	    appId:'xxxxx',//申请的腾讯ai项目app_id
-	    appKey:'xxxx'//申请的腾讯ai项目appkey
+	    appId:'1106995576',
+	    appKey:'abB4qko6kiNPES3B'
 	  };
 class ai{
 	//字典排序
@@ -34,32 +34,6 @@ class ai{
 
 	    return paramStr;
 	}
-	//URL编码处理
-	// URLEncode(clearString){
-	// 	let output = "";
-	//     let x = 0;
-	//     clearString = clearString.toString();
-	//     let regex = /(^[a-zA-Z0-9-_.]*)/;
-	//     while (x < clearString.length) {
-	//         let match = regex.exec(clearString.substr(x));
-	//         if (match != null && match.length > 1 && match[1] != "") {
-	//             output += match[1];
-	//             x += match[1].length;
-	//         } else {
-	//             if (clearString.substr(x, 1) == " ") {
-	//                 //原文在此用 clearString[x] == ' ' 做判断, 但ie不支持把字符串当作数组来访问,
-	//                 //修改后两种浏览器都可兼容
-	//                 output += "+";
-	//             } else {
-	//                 let charCode = clearString.charCodeAt(x);
-	//                 let hexVal = charCode.toString(16);
-	//                 output +="%" + (hexVal.length < 2 ? "0" : "") + hexVal.toUpperCase();
-	//             }
-	//             x++;
-	//         }
-	//     }
-	//     return output;
-	// }
 	//生成随机数
 	randomNumber(){
 		let arrStr='abcdefghijklmnopqrstuvwxyz0123456789'.split('');
@@ -79,79 +53,11 @@ class ai{
 	    let str = this.jsonToURL(json);
 	    str = str.substr(1, str.length);
 	    str += "&" + "app_key" + "=" + config.appKey;
-	    let md5Str = crypto.createHash("md5WithRSAEncryption").update(str || "").digest("hex");
+	    let md5Str = crypto.createHash("md5WithRSAEncryption").update(str || "").digest("hex");//md5加密
 	    let sign=md5Str.toUpperCase();
 	    param.sign=sign;
 	    return param;
 	}
 
-	// ceshi(img){
-	// 	console.log('ok');
-	// 	console.log(this.randomNumber());
-	// 	let submitData={
-	// 		app_id: config.appId,
-	//         image: img,
-	//         model: 8,
-	//         nonce_str: this.randomNumber(),
-	//         time_stamp: Math.floor(new Date().getTime() / 1000)
-	//     };
-	//     // console.log(submitData);
-	// 	console.log(this.getReqSign(submitData));
-	// 	if(submitData["sign"]){
-	// 		delete submitData["sign"];
-	// 	}
-
-	// 	submitData['sign']=this.getReqSign(submitData);
-	// 	console.log('打印参数',submitData['sign']);
-	// 	request.post({
-	// 		url:'https://api.ai.qq.com/fcgi-bin/ptu/ptu_facemerge',
-	// 		form:submitData
-	// 	},function(err,httpResponse,body){
-	// 		// body.data.image
-			
-	// 		body=JSON.parse(body);
-	// 		console.log(typeof(body));
-	// 		console.log(body.msg);
-	// 		let path = './upload/'+ Date.now() +'.png';//从app.js级开始找--在我的项目工程里是这样的
-	//         // let base64 = data.replace(/^data:image\/\w+;base64,/, "");//去掉图片base64码前面部分data:image/png;base64
-	//         let dataBuffer = new Buffer(body.data.image, 'base64'); //把base64码转成buffer对象，
-	//         // console.log('dataBuffer是否是Buffer对象：'+Buffer.isBuffer(dataBuffer));
-	//         fs.writeFile(path,dataBuffer,function(err){//用fs写入文件
-	//             if(err){
-	//                 console.log(err);
-	//             }else{
-	//                console.log('写入成功！');
-	//             }
-	//         })
-	// 	})
-	// 	// request({
-	// 	// 	url:'https://api.ai.qq.com/fcgi-bin/ptu/ptu_facemerge',
-	// 	// 	method:'POST',
-			
-	// 	// 	data:submitData
-	// 	// },function(error, response, body){
-	// 	// 	console.log(body);	
-	// 	// 	// return body;
-	// 	// 	// let path = './upload/'+ Date.now() +'.txt';
-	// 	// 	// fs.writeFile(path,body,function(err){//用fs写入文件
-	// 	//  //        if(err){
-	// 	//  //            console.log(err);
-	// 	//  //        }else{
-	// 	//  //           console.log('写入成功！');
-	// 	//  //        }
-	// 	//  //    })
-	// 	// });
-	// 	// request('https://api.ai.qq.com/fcgi-bin/ptu/ptu_facemerge',function(error, response, body){
-	// 	// 	console.log(error, response, body);
-	// 	// });
-	// 	console.log(submitData.nonce_str,'---',submitData.time_stamp);
-	// 	// return 11;
-	// }
 }
 module.exports=ai;
- // 对象键名排序
- //let dic={x:2,z:1,y:3};//输出  {x:2，y:3，z:1}
- // let sdic=Object.keys(dic).sort();
- // for(ki in sdic){                     
- //    document.writeln(sdic[ki]+":"+dic[sdic[ki]]+",");
- // }
--- src/apiClient.js
+++ src/apiClient.js
@@ -1,7 +1,7 @@
 const txAiJs = require('./TxAiAuthentication.js');
 const txAi = new txAiJs();
 const request=require('request');
-
+const http = require('http');
 class apiClient{
 	//文本翻译，https://api.ai.qq.com/fcgi-bin/nlp/nlp_texttrans, type,text
 	textTranslation(){
@@ -42,21 +42,38 @@ class apiClient{
 		})
 	}
 	// 人脸融合https://api.ai.qq.com/fcgi-bin/ptu/ptu_facemerge
+	// http://pic1.win4000.com/tj/2018-05-08/5af1610ccfb86.jpg
 	ptuFacemerge(){
 		console.log('进入人脸融合');
-		let data=txAi.getReqData({
-			model:1,
-			image:''
+		let url='http://pic1.win4000.com/tj/2018-05-08/5af1610ccfb86.jpg';
+		http.get(url,function(res){
+		　　var chunks = []; //用于保存网络请求不断加载传输的缓冲数据
+		　　var size = 0;　　 //保存缓冲数据的总长度
+		　　res.on('data',function(chunk){
+		　　　　chunks.push(chunk);　 
+		　　　　size += chunk.length;　　//累加缓冲数据的长度
+		　　});
+			res.on('end',function(err){
+		　　　　var data = Buffer.concat(chunks, size);　　//Buffer.concat将chunks数组中的缓冲数据拼接起来，返回一个新的Buffer对象赋值给data
+		　　　　console.log(Buffer.isBuffer(data));　　　　//可通过Buffer.isBuffer()方法判断变量是否为一个Buffer对象
+		　　　　var base64Img = data.toString('base64');　　//将Buffer对象转换为字符串并以base64编码格式显示
+		　　　　console.log(base64Img);　　 //进入终端terminal,然后进入index.js所在的目录，　　　　　　　　　　　　　　　　　　　
+		　　
+				let datas=txAi.getReqData({
+					model:1,
+					image:base64Img
+				});
+				console.log(datas);
+				request.post({
+					url:'https://api.ai.qq.com/fcgi-bin/ptu/ptu_facemerge',//翻译君api
+					form:datas
+				},function(err,httpResponse,body){
+					// return {a:22};
+					console.log(body);
+					return body;
+				})
+			});
 		});
-		console.log(data);
-		request.post({
-			url:'https://api.ai.qq.com/fcgi-bin/ptu/ptu_facemerge',//翻译君api
-			form:data
-		},function(err,httpResponse,body){
-			// return {a:22};
-			console.log(body);
-			return body;
-		})
 	}
 	
 }
